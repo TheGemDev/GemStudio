50d5bcb2afb80f147e02ff3694f45b13dcdc0dfe
The Gem Dev <53819287+the-gem-dev@users.noreply.github.com>
2020-12-13 16:57:56 +0100

    added custom scrollbar

--- app/src/App.js
+++ app/src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <div class="App-header">
-        <Space direction="vertical" size={[0, 100]} wrap>
-          <Button block type="primary" style={{width: 400}}>OPEN A PROJECT</Button>
+        <Space direction="vertical" size={[1, 100]} wrap>
+          <Button block type="primary" style={{width: 350}}>OPEN A PROJECT</Button>
           <Button block type="primary">CREATE A NEW PROJECT</Button>
-          <Button block type="link" href="http://wiki.compilgames.net/doku.php/gdevelop5/start">SEARCH THE DOCUMENTATION</Button>
+          <Button block type="text" style={{color: "#fff"}}href="http://wiki.compilgames.net/doku.php/gdevelop5/start">SEARCH THE DOCUMENTATION</Button>
         </Space>
       </div>
     </div>
