49477b71e24cd946c7323f024a51b8eec6b96650
The Gem Dev <53819287+the-gem-dev@users.noreply.github.com>
2020-12-17 12:54:21 +0100

    modified startpage

---
+++ app/src/components/Modal/index.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {Modal, Button } from 'antd';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/components/Startpage/index.js
+++ app/src/components/Startpage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Space, Button, Upload } from 'antd';
+import { Space, Button, Upload, Typography} from 'antd';
 import './style.css';
 import logo from '../../res/logo/GD-logo.png';
 
@@ -7,14 +7,25 @@ function App() {
   return (
     
     <div className="App-page">
-      <div class="App-header">
-        <Space direction="vertical" size={[1, 10]} >
+      <div className="App-header">
+        
+      </div>
+      <div class="App-body">
+        <Space direction="vertical" size={[1, -10]}>
             <img src={logo} width={400} alt="Laboratoria" />
             <Upload>
               <Button block type="primary" style={{width: 400}}>OPEN A PROJECT</Button>
             </Upload>
             <Button block type="primary">CREATE A NEW PROJECT</Button>
             <Button block type="text" style={{color: "#fff"}}href="http://wiki.compilgames.net/doku.php/gdevelop5/start">SEARCH THE DOCUMENTATION</Button>
+        </Space>
+        
+      </div>
+      <div className="App-footer">
+        <Space size={[-10, 1]}>
+          <Button block type="text" style={{color: "#fff", fontSize: "20" }}>ABOUT GDEVELOP</Button>
+          <Button block type="text" style={{color: "#fff", fontSize: "20" }}>GDEVELOP FORUMS</Button>
+          <Button block type="text" style={{color: "#fff", fontSize: "20" }}>HELP AND TUTORIALS</Button>
         </Space>
       </div>
     </div>
